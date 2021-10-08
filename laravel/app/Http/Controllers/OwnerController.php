a5cb5bc4dbf6047b980c7353a9540872ad8e3b8c
izzudin26 <izzudin.ar26@gmail.com>
2021-10-08 15:19:11 +0700

    add trigger and facilities collection in property

--- laravel/app/Http/Controllers/OwnerController.php
+++ laravel/app/Http/Controllers/OwnerController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -8,7 +9,8 @@ use Session;
 
 class OwnerController extends Controller
 {
-    function compact_curr($n, $presisi=1) {
+    function compact_curr($n, $presisi = 1)
+    {
         if ($n < 900) {
             $format_angka = number_format($n, $presisi);
             $simbol = '';
@@ -25,90 +27,99 @@ class OwnerController extends Controller
             $format_angka = number_format($n / 1000000000000, $presisi);
             $simbol = 'T';
         }
-     
-        if ( $presisi > 0 ) {
-            $pisah = '.' . str_repeat( '0', $presisi );
-            $format_angka = str_replace( $pisah, '', $format_angka );
+
+        if ($presisi > 0) {
+            $pisah = '.' . str_repeat('0', $presisi);
+            $format_angka = str_replace($pisah, '', $format_angka);
         }
-        
+
         return $format_angka . $simbol;
     }
 
-    public function login() {
+    public function login()
+    {
         return view('owner/login');
     }
 
-    public function register() {
+    public function register()
+    {
         return view('owner/register');
     }
 
-    public function dashboard() {
+    public function dashboard()
+    {
         $ownerId = Auth::user()->id;
 
         $data = [
             "propertyCount" => DB::table('property')->where('ownerId', $ownerId)->count(),
             "incomeCount" => $this->compact_curr(
-                             DB::table('bill')->selectRaw('bill.*, property.ownerId')
-                             ->join('property', 'bill.propertyId', '=', 'property.id')
-                             ->join('booking', 'booking.id', '=', 'bill.bookingId')
-                             ->where('ownerId', $ownerId)
-                             ->where('booking.status', 1)
-                             ->where('bill.status', 1)->sum('bill.price')),
+                DB::table('bill')->selectRaw('bill.*, property.ownerId')
+                    ->join('property', 'bill.propertyId', '=', 'property.id')
+                    ->join('booking', 'booking.id', '=', 'bill.bookingId')
+                    ->where('ownerId', $ownerId)
+                    ->where('booking.status', 1)
+                    ->where('bill.status', 1)->sum('bill.price')
+            ),
             "booking" => DB::table('booking')
-                         ->join('property', 'property.id', '=', 'booking.propertyId')
-                         ->where('ownerId', $ownerId)->get()
+                ->join('property', 'property.id', '=', 'booking.propertyId')
+                ->where('ownerId', $ownerId)->get()
 
         ];
 
         return view('owner/dashboard', $data);
     }
 
-    public function chat() {
+    public function chat()
+    {
         return view('owner/chat');
     }
 
-    public function income() {
+    public function income()
+    {
         $ownerId = Auth::user()->id;
         $data = [
             "income" =>  DB::table('bill')
-                         ->join('property', 'property.id', '=', 'bill.propertyId')
-                         ->join('q_bill', 'q_bill.id', '=', 'bill.id')
-                         ->join('booking', 'booking.id', '=', 'bill.bookingId')
-                         ->where('ownerId', $ownerId)
-                         ->where('bill.status', 1)
-                         ->where('booking.status', 1)
-                         ->orderBy('bill.date')->get()
+                ->join('property', 'property.id', '=', 'bill.propertyId')
+                ->join('q_bill', 'q_bill.id', '=', 'bill.id')
+                ->join('booking', 'booking.id', '=', 'bill.bookingId')
+                ->where('ownerId', $ownerId)
+                ->where('bill.status', 1)
+                ->where('booking.status', 1)
+                ->orderBy('bill.date')->get()
         ];
         return view('owner/income', $data);
     }
 
-    public function withdraw() {
+    public function withdraw()
+    {
         $ownerId = Auth::user()->id;
         return view('owner/income', $data);
     }
 
-    public function booking() {
+    public function booking()
+    {
         $ownerId = Auth::user()->id;
         $data = [
             "booking" => DB::table('booking')
-                         ->join('property', 'property.id', '=', 'booking.propertyId')
-                         ->where('ownerId', $ownerId)->get()
+                ->join('property', 'property.id', '=', 'booking.propertyId')
+                ->where('ownerId', $ownerId)->get()
         ];
 
         return view('owner/booking', $data);
     }
 
-    public function property() {
+    public function property()
+    {
         $ownerId = Auth::user()->id;
         $data = [
             "property" => DB::table('property')->where('ownerId', $ownerId)->get()
         ];
-
-        return view('owner/property', $data);
-
+        $facilities = DB::table('facility_list')->get();
+        return view('owner/property', ['data' => $data, 'facilities' => $facilities]);
     }
 
-    public function bill() {
+    public function bill()
+    {
         $ownerId = Auth::user()->id;
         $property = DB::table('q_property')->where('ownerId', $ownerId)->get();
         $bill = DB::table('q_bill')->where('propertyId', $property[0]->id)->get();
@@ -118,7 +129,8 @@ class OwnerController extends Controller
         return view('owner/bill', $data);
     }
 
-    public function insertProperty(Request $req) {
+    public function insertProperty(Request $req)
+    {
         DB::table('property')->insert([
             "vrooms" => $req->vrooms,
             "ownerId" => Auth::user()->id,
@@ -137,5 +149,50 @@ class OwnerController extends Controller
 
         Session::flash('success', 'Properti telah ditambahkan');
         return redirect()->back();
+    }
+
+    public function getFacilitiesProperty($propertyId)
+    {
+        $facilitiesProperty = DB::table('facility')
+            ->where('propertyId', $propertyId)
+            ->join('facility_list', 'facility.facilityId', '=', 'facility_list.id')
+            ->join('property', 'facility.propertyId', '=', 'property.id')
+            ->select(
+                'property.name as name',
+                'facility_list.name as facility',
+                'facility_list.webIcon as webIcon',
+                'facility_list.id as facilityId'
+            )
+            ->get();
+        $facilities = DB::table('facility_list')->get();
+        return ['facilitiesProperty' => $facilitiesProperty, 'facilities' => $facilities];
+    }
+
+    public function triggerFacilityProperty(Request $req, $propertyId)
+    {
+        $isPropertyHaveFacility = DB::table('facility')
+        ->where('propertyId', $propertyId)
+        ->where('facilityId', $req->facilityId)
+        ->first();
+        $message = '';
+
+        if($isPropertyHaveFacility){
+            DB::table('facility')
+            ->where('propertyId', $propertyId)
+            ->where('facilityId', $req->facilityId)
+            ->delete();
+            $message = 'Trigger Deleted';
+        }else{
+            DB::table('facility')->insert([
+                'propertyId' => $propertyId,
+                'facilityId' => $req->facilityId
+            ]);
+            $message = 'Trigger Created';
+        }
+
+        return response()->json(
+            ['message' => $message],
+            200
+        );
     }
 }
