d3c90371f2d28be51d539b07625570e781b10b7c
izzudin26 <izzudin.ar26@gmail.com>
2021-10-11 10:39:17 +0700

    update view

--- laravel/resources/views/owner/booking.blade.php
+++ laravel/resources/views/owner/booking.blade.php
@@ -38,7 +38,7 @@
             <div class="card">
                 <div class="card-body">
                     <h4 class="card-title">Sewa</h4>
-                    <h6 class="card-subtitle">Kelola Sewa property anda</h6>
+                    <h6 class="card-subtitle">Kelola Sewa anda</h6>
                     <div class="table-responsive">
                         <table id="datatable" class="table table-striped table-bordered no-wrap">
                             <thead>
@@ -46,6 +46,7 @@
                                     <th>No</th>
                                     <th>Properti</th>
                                     <th>Tanggal</th>
+                                    <th>Status</th>
                                     <th>Action</th>
                                 </tr>
                             </thead>
@@ -56,10 +57,21 @@
                                   <td>{{ $data->name }}</td>
                                   <td>{{ $data->date }}</td>
                                   <td>
-                                    <button data-id="{{ $data->userId }}" class="btn btn-primary btn-circle btn-sm viewUser"><i class="fa fa-user"></i></button>
-                                    <button class="btn btn-success btn-circle btn-sm"><i class="fa fa-check"></i></button>
-                                    <button class="btn btn-danger btn-circle btn-sm"><i class="fa fa-times"></i></button>
+                                    @if($data->status == 0)
+                                          Pending
+                                    @elseif($data->status == 1)
+                                        diterima
+                                    @else
+                                        ditolak
+                                    @endif
                                   </td>
+                                  @if($data->status == 0)
+                                  <td>
+                                    <button onclick="fetchInfo({{ $data->id }})" data-toggle="modal" data-target="#showUser" class="btn btn-primary btn-circle btn-sm viewUser"><i class="fa fa-user"></i></button>
+                                    <button data-toggle="modal" data-target="#confirmationModal" onclick="confirmation({{$data->id}})" class="btn btn-success btn-circle btn-sm"><i class="fa fa-check"></i></button>
+                                    <button data-toggle="modal" data-target="#confirmationDenyModal" onclick="denyConfirmation({{$data->id}})" class="btn btn-danger btn-circle btn-sm"><i class="fa fa-times"></i></button>
+                                  </td>
+                                  @endif
                               </tr>
                               @endforeach
                             </tbody>
@@ -74,4 +86,135 @@
 <!-- ============================================================== -->
 <!-- End Page wrapper  -->
 <!-- ============================================================== -->
+<div class="modal fade" id="showUser" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="myCenterModalLabel">Informasi Calon Penghuni</h4>
+                <button type="button" class="close" data-dismiss="modal"
+                    aria-hidden="true">×</button>
+            </div>
+            <div id="userInfo" method="POST">
+                <img src="" class="col-12 p-5" alt="" id="profileImage">
+                <table class="table table-responsive">
+                    <tr>
+                        <th>Nama</th>
+                        <th>:</th>
+                        <th><span id="name"></span></th>
+                    </tr>
+                    <tr>
+                        <th>Email</th>
+                        <th>:</th>
+                        <th><span id="email"></span></th>
+                    </tr>
+                    <tr>
+                        <th>Nomor Telepon</th>
+                        <th>:</th>
+                        <th><span id="number"></span></th>
+                    </tr>
+                    <tr>
+                        <th>Alamat</th>
+                        <th>:</th>
+                        <th><span id="address"></span></th>
+                    </tr>
+                </table>
+            <div class="row col-md-12 mx-auto">
+            <div class="form-group col-md-12">
+                <button type="button" class="btn btn-primary btn-block"  data-dismiss="modal" aria-hidden="true">Tutup</button>
+              </div>
+            </div>
+        </div>
+        </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="myCenterModalLabel">Konfirmasi Penyewaan</h4>
+                <button type="button" class="close" data-dismiss="modal"
+                    aria-hidden="true">×</button>
+            </div>
+            <form action="" id="acceptBookingForm" method="POST">
+                
+              @csrf
+              <input type="hidden" name="status" value="accepted">
+
+            <div class="p-3">
+            Yakin ingin menyutujuhi penyewaan <span id="bookingDetail"></span> ?
+            </div>
+            <div class="row col-md-12 mx-auto">
+            <div class="form-group col-md-6">
+                <button type="submit" class="btn btn-primary btn-block"  data-dismiss="modal" aria-hidden="true">Tidak</button>
+              </div>
+              <div class="form-group col-md-6">
+                <button type="submit" class="btn btn-success btn-block">Ya</button>
+              </div>
+            </div>
+        </form>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="confirmationDenyModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="myCenterModalLabel">Konfirmasi Penyewaan</h4>
+                <button type="button" class="close" data-dismiss="modal"
+                    aria-hidden="true">×</button>
+            </div>
+            <form action="" id="denyBookingForm" method="POST">
+              @csrf
+              <input type="hidden" name="status" value="deny">
+            <div class="p-3">
+            Yakin ingin menolak penyewaan <span id="bookingDetailDeny"></span> ?
+            </div>
+            <div class="row col-md-12 mx-auto">
+            <div class="form-group col-md-6">
+                <button type="submit" class="btn btn-primary btn-block"  data-dismiss="modal" aria-hidden="true">Tidak</button>
+              </div>
+              <div class="form-group col-md-6">
+                <button type="submit" class="btn btn-danger btn-block">Tolak</button>
+              </div>
+            </div>
+        </form>
+        </div>
+    </div>
+</div>
+
+<script>
+    const currentUrl = window.location.pathname
+    function fetchInfo(bookingId){
+        fetch(`${currentUrl}/information/${bookingId}`)
+        .then(res => res.json())
+        .then(res => {
+            document.querySelector('#name').innerText = res.user.name
+            document.querySelector('#email').innerText = res.user.email
+            document.querySelector('#number').innerText = res.user.phone
+            document.querySelector('#address').innerText = res.user.address
+            document.querySelector('#profileImage').setAttribute("src", res.user.face)
+        })
+    }
+
+    function confirmation(bookingId){
+        document.querySelector('#acceptBookingForm').setAttribute('action', `${currentUrl}/confirmation/${bookingId}`)
+        fetch(`${currentUrl}/information/${bookingId}`)
+        .then(res => res.json())
+        .then(res => {
+            document.querySelector('#bookingDetail').innerText = `${res.property.name} oleh ${res.user.name}`
+        })
+    }
+
+    function denyConfirmation(bookingId){
+        document.querySelector('#denyBookingForm').setAttribute('action', `${currentUrl}/confirmation/${bookingId}`)
+        fetch(`${currentUrl}/information/${bookingId}`)
+        .then(res => res.json())
+        .then(res => {
+            document.querySelector('#bookingDetailDeny').innerText = `${res.property.name} oleh ${res.user.name}`
+        })
+    }
+    
+</script>
 @endsection
