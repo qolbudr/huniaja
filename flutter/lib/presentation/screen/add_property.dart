fe029afaa8bcee283a327860293c234aada36a96
izzudin26 <izzudin.ar26@gmail.com>
2021-10-17 04:37:22 +0700

    undo add_property.dart

--- flutter/lib/presentation/screen/add_property.dart
+++ flutter/lib/presentation/screen/add_property.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:manpro/model/auth_login.dart';
-import 'package:manpro/model/facility.dart';
 import 'package:manpro/model/location_item.dart';
 import 'package:manpro/presentation/screen/owner/location_picker.dart';
 import 'package:manpro/presentation/widget/input.dart';
@@ -11,7 +10,6 @@ import 'package:manpro/presentation/widget/snackbar.dart';
 import 'package:manpro/provider/auth.dart';
 import 'package:manpro/service/api_service.dart';
 import 'package:provider/provider.dart';
-import 'package:image_picker/image_picker.dart';
 
 class AddProperty extends StatefulWidget {
   @override
@@ -19,10 +17,6 @@ class AddProperty extends StatefulWidget {
 }
 
 class _AddPropertyState extends State<AddProperty> {
-  AuthLogin currentAuth;
-
-final ImagePicker _picker = ImagePicker();
-
   final name = TextEditingController();
   final description = TextEditingController();
   final address = TextEditingController();
@@ -36,12 +30,7 @@ final ImagePicker _picker = ImagePicker();
   bool isLoading = false;
   String _currentType = '0';
 
-  List<Facility> facilities;
-
-  XFile image;
-
-  @override
-  void dispose() {
+  @override void dispose() {
     name.dispose();
     description.dispose();
     address.dispose();
@@ -62,111 +51,25 @@ final ImagePicker _picker = ImagePicker();
 
   void openLocationPicker(_) async {
     final location = await Navigator.push(
-        _, MaterialPageRoute(builder: (_) => LocationPicker()));
+      _, MaterialPageRoute(builder: (_) => LocationPicker())
+    );
     updateLocation(location);
   }
 
-  void getFacilities(token, AuthLogin login) async {
-    List<Facility> facilitiesFromService =
-        await ApiService().getFacility(token, login.user.id.toString());
-    setState(() {
-      facilities = facilitiesFromService;
-    });
-  }
-
-  void pickImage()async {
-    XFile img = await _picker.pickImage(source: ImageSource.gallery);
-    print(img.path);
-    setState(() {
-      image = img;
-    });
-  }
-
-  Widget showFacility() {
-    return Container(
-      padding: EdgeInsets.only(top: 20),
-      child: Column(
-        children: [
-          Text(
-            "Fasilitas",
-            style: TextStyle(
-              fontSize: 18,
-            ),
-          ),
-          Column(
-              children: facilities
-                  .map((e) => CheckboxListTile(
-                        secondary: Icon(IconData(e.mobileIcon,
-                            fontFamily: 'MaterialIcons')),
-                        title: Text(e.name),
-                        value: e.isChecked,
-                        onChanged: (bool value) {
-                          setValue(e.facilityId, value);
-                        },
-                      ))
-                  .toList()),
-        ],
-      ),
-    );
-  }
-
-  Widget addImagePicker(){
-    return GestureDetector(
-      onTap: (){
-        print("hit");
-        pickImage();
-      },
-      child: Container(
-        margin: EdgeInsets.only(
-          top: 20
-        ),
-        height: MediaQuery.of(context).size.height * 0.2,
-        
-        decoration: BoxDecoration(
-          color: Colors.transparent,
-          border: Border.all(width: 3.0, color: Colors.blue),
-          borderRadius: BorderRadius.circular(10.0)
-        ),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.add, color: Colors.blue,),
-              Text("Tambahkan Foto", style: TextStyle(
-                color: Colors.blue
-              ),)
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  void setValue(int id, bool value) {
-    Facility f = facilities.where((f) => f.facilityId == id).toList()[0];
-    int index = facilities.indexOf(f);
-    setState(() {
-      facilities[index].isChecked = value;
-    });
-  }
-
   Widget build(BuildContext context) {
-    return Consumer<AuthNotifier>(builder: (context, _, child) {
-      AuthLogin login = _.authLogin;
-      if (facilities == null) {
-        getFacilities(login.token, login);
-      }
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Tambah Property"),
-          automaticallyImplyLeading: false,
-          elevation: 0,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 15),
-              child: Center(
-                child: Link(
+    return Consumer<AuthNotifier>(
+      builder: (context, _, child) {
+        AuthLogin login = _.authLogin;
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Tambah Property"),
+            automaticallyImplyLeading: false,
+            elevation: 0,
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 15),
+                child: Center(
+                  child: Link(
                     text: "Selanjutnya",
                     onTap: () async {
                       try {
@@ -187,71 +90,62 @@ final ImagePicker _picker = ImagePicker();
                       } catch (e) {
                         return showSnackbar(context, e.toString());
                       }
-                    }),
-              ),
-            )
-          ],
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Container(
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SizedBox(height: 10),
-                        Input(hintText: "Nama", controller: name),
-                        SizedBox(height: 10),
-                        TextArea(
-                            hintText: "Deskripsi", controller: description),
-                        SizedBox(height: 10),
-                        SizedBox(height: 10),
-                        Input(
-                            hintText: "Virtual Room", controller: virtualRoom),
-                        SizedBox(height: 10),
-                        Input(
-                            hintText: "Harga (Harian)", controller: priceDaily),
-                        SizedBox(height: 10),
-                        Input(
-                            hintText: "Harga (Bulanan)",
-                            controller: priceMonthly),
-                        SizedBox(height: 10),
-                        Input(
-                            hintText: "Harga (Tahunan)",
-                            controller: priceYearly),
-                        SizedBox(height: 10),
-                        Select(
-                          icon: Icons.hotel,
-                          items: [
-                            {'label': 'Rumah', 'value': '0'},
-                            {'label': 'Apartemen', 'value': '1'},
-                            {'label': 'Kontrakan', 'value': '2'},
-                          ],
-                          currentSelectedValue: _currentType,
-                          text: "Tipe",
-                          onChanged: (value) {
-                            setState(() {
-                              _currentType = value;
-                            });
-                          },
-                        ),
-                        ((facilities != null) ? showFacility() : Container(
-                          padding: EdgeInsets.only(top: 30),
-                          child: CircularProgressIndicator(),
-                        )),
-                        addImagePicker()
-                      ],
+                    }
+                  ),
+                ),
+              )
+            ],
+          ),
+          body: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 10),
+                          Input(hintText: "Nama", controller: name),
+                          SizedBox(height: 10),
+                          TextArea(hintText: "Deskripsi", controller: description),
+                          SizedBox(height: 10),
+                          Input(hintText: "Alamat", readOnly: true, controller: address, onTap: () => openLocationPicker(context)),
+                          SizedBox(height: 10),
+                          Input(hintText: "Virtual Room",  controller: virtualRoom),
+                          SizedBox(height: 10),
+                          Input(hintText: "Harga (Harian)", controller: priceDaily),
+                          SizedBox(height: 10),
+                          Input(hintText: "Harga (Bulanan)", controller: priceMonthly),
+                          SizedBox(height: 10),
+                          Input(hintText: "Harga (Tahunan)", controller: priceYearly),
+                          SizedBox(height: 10),
+                          Select(
+                            icon: Icons.hotel,
+                            items: [
+                              {'label':'Rumah', 'value':'0'},
+                              {'label':'Apartemen', 'value':'1'},
+                              {'label':'Kontrakan', 'value':'2'},
+                            ],
+                            currentSelectedValue: _currentType,
+                            text: "Tipe",
+                            onChanged: (value) {
+                              setState(() {
+                                _currentType = value;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            )
-          ],
-        ),
-      );
-    });
+              )
+            ],
+          ),
+        );
+      }
+    );
   }
 }
