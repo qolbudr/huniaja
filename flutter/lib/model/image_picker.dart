105631d8bdff3199d077f2a599c09baee8e43b5b
izzudin26 <izzudin.ar26@gmail.com>
2021-10-17 07:54:31 +0700

    add model image picker

--- flutter/lib/model/image_picker.dart
+++ flutter/lib/model/image_picker.dart
@@ -1,13 +1,16 @@
-class ImagePicker {
+class ImagePickerApp {
   final String imageName;
   final String path;
+  final int id;
 
-  ImagePicker.fromJson(Map<String, dynamic> json)
+  ImagePickerApp.fromJson(Map<String, dynamic> json)
   : imageName = json['imageName'],
-  path= json['path'];
+  path= json['path'],
+  id = json['id'];
 
   Map<String, dynamic> toJson() => {
     'imageName': imageName,
-    'path': path
+    'path': path,
+    'id': id
   };
 }
